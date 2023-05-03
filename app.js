f8a146ee82691aefb8937fffc9172025b118bcd3
Leonardo Cazotto Stuber <leonardo_stu@outlook.com.br>
2023-05-03 13:10:03 +0100

    #3 Publicação do Domínio no Azure AD

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var authRouter = require('./routes/auth');
+var wellKnownRouter = require('./routes/wellKnown');
 
 // initialize express
 var app = express();
@@ -45,6 +46,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/auth', authRouter);
+app.use('/.well-known/microsoft-identity-association.json', wellKnownRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {

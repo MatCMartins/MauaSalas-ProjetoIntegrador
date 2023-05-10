87304354d8359b4f4295824f3051b72f7a69323d
Leonardo Cazotto Stuber <leonardo_stu@outlook.com.br>
2023-05-10 03:02:12 +0100

    #39 Correção de Bug na Mensagem de Erro

--- app.js
+++ app.js
@@ -63,7 +63,12 @@ app.use(function (err, req, res, next) {
 
     // render the error page
     res.status(err.status || 500);
-    res.render('error');
+    res.render('error', {
+        title: 'Mauá Salas - Erro',
+        isAuthenticated: req.session.isAuthenticated,
+        username: req.session.account && req.session.account.name,
+        erro: (err.status || 500),
+    });
 });
 
 module.exports = app;

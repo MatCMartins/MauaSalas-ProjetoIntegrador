81c3fc51a0b73026865694933653fdb1f3ff1acb
Leonardo Cazotto Stuber <leonardo_stu@outlook.com.br>
2023-07-05 21:20:57 -0300

    Update Login Redirects

--- routes/admin.js
+++ routes/admin.js
@@ -9,7 +9,7 @@ var createError = require('http-errors');
 
 function isAuthenticated(req, res, next) {
     if (!req.session.isAuthenticated) {
-        return res.redirect('/auth/signin'); // redirect to sign-in route
+        return res.redirect(`/auth/signin?url=${req.baseUrl+req.url}`); // redirect to sign-in route
     }
     else if (req.session.userType != 1) {
         next(createError(403));
